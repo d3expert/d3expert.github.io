204625ad41f1c4ffe58db47b6300e0bfb154b8e5
webmaster444 <jlmobile710@gmail.com>
2020-07-23 11:59:22 +0300

    single page scroll effect

--- js/theme.js
+++ js/theme.js
@@ -21,6 +21,58 @@
 	}
 	navbarFixed();
 
+	$('.nav-link').on('click', function(e){
+		e.preventDefault(true);		
+		let target = $(this).attr('href');
+		if(target=="#portfolio"||target=="#service"){
+			$('html,body').animate({
+				scrollTop: $(target).offset().top - 80
+			}, 1000);
+		}else{
+			$('html,body').animate({
+				scrollTop: $(target).offset().top
+			}, 1000);
+		}		
+		$('.nav-item').removeClass('active');
+		$('.nav-link[href="'+target+'"]').closest('.nav-item').addClass('active');
+	})
+
+	// Cache selectors
+var lastId,
+topMenu = $("div.main_menu"),
+topMenuHeight = topMenu.outerHeight()+1,
+// All list items
+menuItems = topMenu.find("a"),
+// Anchors corresponding to menu items
+scrollItems = menuItems.map(function(){
+  var item = $($(this).attr("href"));
+   if (item.length) { return item; }
+});
+
+
+// Bind to scroll
+$(window).scroll(function(){
+  // Get container scroll position
+  var fromTop = $(this).scrollTop()+topMenuHeight;
+  
+  // Get id of current scroll item
+  var cur = scrollItems.map(function(){
+	if ($(this).offset().top < fromTop)
+	  return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length-1];
+  var id = cur && cur.length ? cur[0].id : "";
+  
+  if (lastId !== id) {
+	  	lastId = id;
+	  	// Set/remove active class
+	  	console.log(menuItems);
+	  	menuItems.parent().removeClass("active");
+		menuItems.filter("[href='#"+id+"']").parent().addClass("active");
+  }                   
+});
+
 	// $('select').niceSelect();
 	/* ---------------------------------------------
             Isotope js Starts
